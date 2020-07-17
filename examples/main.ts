7844b03e3f4262248e904ad00c75f178be880f71
Milesq <wmilosz88@gmail.com>
2020-07-17 22:34:59 +0200

    Add basic example

---
+++ examples/main.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+import createJWTProtector from '../src/lib';
+
+const app = express();
+const jwtProtector = createJWTProtector();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/secret', jwtProtector, (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(8080, console.log.bind(null, 'Server is running'));
