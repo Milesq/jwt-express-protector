fd0edba31aef50c70dd8ed9eb1f7bf097a9b3c86
Milesq <wmilosz88@gmail.com>
2020-07-18 21:09:22 +0200

    Add basic tests

--- __tests__/main.ts
+++ __tests__/main.ts
@@ -1,6 +1,43 @@
-import isPrime from '../src/main';
+import express from 'express'
+import request from 'supertest'
+import createJWTProtector from '../src/lib'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(isPrime(9)).toBe(false);
-  expect(isPrime(11)).toBe(true);
-});
+describe('createJWTProtector', () => {
+  let app: express.Application
+
+  const verifyToken = jest.fn((payload: object) => {
+    return {
+      userName: payload,
+      admin: false,
+    }
+  })
+
+  beforeEach(() => {
+    app = express()
+
+    const jwtProtector = createJWTProtector({
+      secret: 'abc',
+      verifyToken,
+    })
+
+    app.get('/secret', jwtProtector, (req, res) => {
+      res.send('content')
+    })
+  })
+
+  it('verifyToken is called', () => {
+    expect(verifyToken).toBeCalled()
+    expect(3).toBe(3)
+  })
+
+  it('returns 401 when unauthorized', async () => {
+    await request(app).get('/secret').expect(401)
+  })
+
+  it('returns 200 when authorized', async () => {
+    await request(app)
+      .get('/secret')
+      .auth('', { type: 'bearer' })
+      .expect(200)
+  })
+})
